be20906b912d2cddcef4db893f349c5b21b69cda
air <acesparkle243@gmail.com>
2026-04-23 14:15:16 +0800

    court controller

--- backend/src/controllers/court.controller.ts
+++ backend/src/controllers/court.controller.ts
@@ -1,10 +1,110 @@
 import type { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
+import type { Params } from "./community.controller.js";
 
 export const createMatchCourt = async (
-  request: Request,
+  request: Request<Params>,
   response: Response,
 ) => {
   try {
+    const { communityId, hostId } = request.params;
+    const user = request.user;
+    if (!user)
+      return response
+        .status(401)
+        .json({ success: false, message: "Unauthorized" });
+
+    if (!communityId || !hostId)
+      return response
+        .status(404)
+        .json({ success: false, message: "Community not found" });
+
+    const community = await prisma.community.findFirst({
+      where: { id: communityId, adminId: user.sub },
+      select: { id: true },
+    });
+
+    if (!community)
+      return response
+        .status(404)
+        .json({ success: false, message: "Community not found" });
+
+    const host = await prisma.host.findFirst({
+      where: { id: hostId, communityId: community.id },
+      select: { id: true },
+    });
+
+    if (!host)
+      return response
+        .status(404)
+        .json({ success: false, message: "Host not found" });
+
+    const court = await prisma.match.create({
+      data: {
+        hostId: host.id,
+        name: `Court ${(await prisma.match.count()) + 1}`,
+      },
+    });
+
+    return response
+      .status(201)
+      .json({ success: true, message: "Court created", court });
+  } catch (error) {
+    console.error("Error creating match court host:", error);
+    return response.status(500).json({
+      success: false,
+      message: error instanceof Error ? error.message : "Internal server error",
+    });
+  }
+};
+
+export const createQueueCourt = async (
+  request: Request<Params>,
+  response: Response,
+) => {
+  try {
+    const { communityId, hostId } = request.params;
+    const user = request.user;
+    if (!user)
+      return response
+        .status(401)
+        .json({ success: false, message: "Unauthorized" });
+
+    if (!communityId || !hostId)
+      return response
+        .status(404)
+        .json({ success: false, message: "Community not found" });
+
+    const community = await prisma.community.findFirst({
+      where: { id: communityId, adminId: user.sub },
+      select: { id: true },
+    });
+
+    if (!community)
+      return response
+        .status(404)
+        .json({ success: false, message: "Community not found" });
+
+    const host = await prisma.host.findFirst({
+      where: { id: hostId, communityId: community.id },
+      select: { id: true },
+    });
+
+    if (!host)
+      return response
+        .status(404)
+        .json({ success: false, message: "Host not found" });
+
+    const queue = await prisma.queue.create({
+      data: {
+        hostId: host.id,
+        name: `Queue ${(await prisma.match.count()) + 1}`,
+      },
+    });
+
+    return response
+      .status(201)
+      .json({ success: true, message: "Court created", queue });
   } catch (error) {
     console.error("Error creating match court host:", error);
     return response.status(500).json({
