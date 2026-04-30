4ca282548940fb2e5c59ea46b5e878f7b292221d
air <acesparkle243@gmail.com>
2026-04-30 17:07:21 +0800

    added ban in frontend

--- frontend/src/components/host_components/PlayerDropdown.tsx
+++ frontend/src/components/host_components/PlayerDropdown.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import type { PlayerType } from "../../pages/host_pages/Match";
+import { useParams } from "react-router-dom";
 
 // type PlayerType
 
@@ -11,6 +13,24 @@ export default function PlayerSettingsDropdown({
   hostedPlayerId,
   player,
 }: PlayerDropdownProps) {
+  const { communityId, hostId } = useParams();
+  const banAPI = async () => {
+    try {
+      await axios.post(
+        `http://localhost:4000/api/private/actions/ban/community/${communityId}/hosts/${hostId}/${player.id}`,
+        {},
+        { withCredentials: true },
+      );
+    } catch (error) {
+      if (axios.isAxiosError(error)) console.error(error);
+      else console.error(error);
+    }
+  };
+
+  const handleBanClick = async () => {
+    await banAPI();
+  };
+
   return (
     <div className="absolute top-7 left-0 boZrder p-2 z-50 bg-white rounded-md w-[168px] cursor-default border grid gap-y-2">
       <div>
@@ -20,7 +40,10 @@ export default function PlayerSettingsDropdown({
         <h4 className="font-semibold">{player.username}</h4>
       </div>
       <div>
-        <button className="w-full block bg-red-500 text-white border-none cursor-pointer hover:bg-red-700 rounded py-1 px-2">
+        <button
+          onClick={handleBanClick}
+          className="w-full block bg-red-500 text-white border-none cursor-pointer hover:bg-red-700 rounded py-1 px-2"
+        >
           Ban
         </button>
       </div>
