42c16b25d455a126113e4c237abdf9ec95de6134
air <acesparkle243@gmail.com>
2026-05-04 10:30:31 +0800

    added vercel fix

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,3 +1,4 @@
-import app from "../backend/src/app.js";
-
-export default app;
+export default async function handler(request: any, response: any) {
+  const { default: app } = await import("../backend/src/app.js");
+  return app(request, response);
+}
