e5ad566c838fad89ed2fe5c41bf4e522968c0645
air <acesparkle243@gmail.com>
2026-05-09 11:42:30 +0800

    new default profile

---
+++ backend/prisma/migrations/20260509034202_new_default_profile/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Account" ALTER COLUMN "profileUrl" SET DEFAULT 'https://static.vecteezy.com/system/resources/previews/030/750/807/non_2x/user-icon-in-trendy-outline-style-isolated-on-white-background-user-silhouette-symbol-for-your-website-design-logo-app-ui-illustration-eps10-free-vector.jpg';
+
+-- AlterTable
+ALTER TABLE "Community" ALTER COLUMN "profileUrl" SET DEFAULT 'https://static.vecteezy.com/system/resources/previews/030/750/807/non_2x/user-icon-in-trendy-outline-style-isolated-on-white-background-user-silhouette-symbol-for-your-website-design-logo-app-ui-illustration-eps10-free-vector.jpg';
---
+++ frontend/src/components/ProfileBar.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+
+export default function ProfileBar() {
+  const { user } = useAuth();
+  const [openProfile, setOpenProfile] = useState<boolean>(false);
+  const profileRef = useRef<HTMLDivElement | null>(null);
+
+  // CLOSE WHEN CLICK OUTSIDE
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        profileRef.current &&
+        !profileRef.current.contains(event.target as Node)
+      ) {
+        setOpenProfile(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+  return (
+    <div className="relative" ref={profileRef}>
+      <button
+        type="button"
+        onClick={() => setOpenProfile((prev) => !prev)}
+        className="flex w-full cursor-pointer items-center gap-3 rounded-2xl px-3 py-2.5 text-left text-[#0c090c] transition-all duration-200 hover:bg-[#fff4df]"
+      >
+        <div className="h-8 w-8 overflow-hidden rounded-xl">
+          <img
+            src={user?.profileUrl}
+            alt={user?.username}
+            className="block h-full w-full object-cover object-center"
+          />
+        </div>
+
+        <div>
+          <span className="block text-sm font-medium">{user?.username}</span>
+        </div>
+      </button>
+
+      <ProfileDropdown open={openProfile} />
+    </div>
+  );
+}
+
+import { RiLogoutBoxLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+
+type ProfileDropdownProps = {
+  open: boolean;
+};
+
+export function ProfileDropdown({ open }: ProfileDropdownProps) {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/login");
+  };
+
+  return (
+    <div
+      className={`absolute bottom-16 left-0 w-full origin-bottom rounded-2xl border border-orange-100 bg-white p-2 shadow-lg transition-all duration-200 ${
+        open
+          ? "pointer-events-auto scale-100 opacity-100"
+          : "pointer-events-none scale-95 opacity-0"
+      }`}
+    >
+      <button
+        type="button"
+        onClick={handleLogout}
+        className="flex w-full cursor-pointer items-center gap-3 rounded-xl px-3 py-2.5 text-sm font-medium text-[#0c090c] transition-all duration-200 hover:bg-[#fff4df] hover:text-[#ff6900]"
+      >
+        {/* ICON */}
+        <div className="flex h-8 w-8 items-center justify-center rounded-xl bg-[#fff4df] text-[#ff6900]">
+          <RiLogoutBoxLine size={16} />
+        </div>
+
+        {/* LABEL */}
+        <span>Logout</span>
+      </button>
+    </div>
+  );
+}
